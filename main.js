32410dc6b844159196ffa9349cf9874059dbb8f3
makhmudoff <gachon031698@gmail.com>
2023-04-25 12:26:41 +0900

    No commit

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 'use strict';
 setInterval(() => {
-  d = new Date(); //object of date()
-  hr = d.getHours();
-  min = d.getMinutes();
-  sec = d.getSeconds();
-  hr_rotation = 30 * hr + min / 2; //converting current time
-  min_rotation = 6 * min;
-  sec_rotation = 6 * sec;
+  let d = new Date(); //object of date()
+  let hr = d.getHours();
+  let min = d.getMinutes();
+  let sec = d.getSeconds();
+  let hr_rotation = 30 * hr + min / 2; //converting current time
+  let min_rotation = 6 * min;
+  let sec_rotation = 6 * sec;
+
+  const hour = document.querySelector('#hour');
+  const minute = document.querySelector('#minute');
+  const second = document.querySelector('#second');
 
   hour.style.transform = `rotate(${hr_rotation}deg)`;
   minute.style.transform = `rotate(${min_rotation}deg)`;
